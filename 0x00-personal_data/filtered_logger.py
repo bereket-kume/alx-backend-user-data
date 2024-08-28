f7ec8a49d87945268729e278996ec30549313005
bereket-kume <bereketkume@gmail.com>
2024-08-28 22:54:03 +0300

    error fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""Module for handling personal data"""
 from typing import List
 import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
     pattern = '|'.join([f"{field}=.*?(?={separator}|$)" for field in fields])
     return re.sub(pattern, lambda m: m.group(0).split('=')[0] + f'={redaction}', message)
