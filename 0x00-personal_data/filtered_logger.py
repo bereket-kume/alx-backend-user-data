c4db6cbafed136a9e9624fb11180f7a3eddb4262
bereket-kume <bereketkume@gmail.com>
2024-08-28 22:46:06 +0300

    filter datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,13 +6,8 @@ from typing import List
 import re
 
 
-def filter_datum(
-        fields: List[str], redaction: str,
-        message: str, seprator: str
-) -> str:
-    """
-    function returns the log message obfuscated
-    """
+def filter_datum(fields: List[str], redaction: str, message: str, seprator: str) -> str:
+    """log message obfuscated """
     for i in fields:
         message = re.sub(f'{i}=.*?{seprator}',
                          f'{i}={redaction}{seprator}', message)
