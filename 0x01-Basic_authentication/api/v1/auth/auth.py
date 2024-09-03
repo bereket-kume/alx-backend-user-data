1b427ec1155859b778cfbe1d56f473eefed3f422
bereket-kume <bereketkume@gmail.com>
2024-09-03 15:15:41 +0300

    fix error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,16 +12,21 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        function used as auth
+        determine if given path require authentication
         """
         if path is None:
             return True
+
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        if path in excluded_paths:
-            return False
-        else:
-            return True
+
+        path = path.rstrip('/')
+        for excluded_path in excluded_paths:
+            excluded_path = excluded_path.rstrip('/')
+            if excluded_path == path or path.startswith(excluded_path + '/'):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
