0c8ca6647beb9cd861895929a6f82d78449590e3
bereket-kume <bereketkume@gmail.com>
2024-09-03 23:19:23 +0300

    basic base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Basic auth module
 """
+import base64
 from .auth import Auth
 
 
@@ -21,3 +22,19 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+    ) -> str:
+        """
+        decode base64 authorization header
+        """
+        if base64_authorization_header is None or\
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decode_val = base64.b64decode(base64_authorization_header)
+            val = decode_val.decode('utf-8')
+            return val
+        except Exception:
+            return None
