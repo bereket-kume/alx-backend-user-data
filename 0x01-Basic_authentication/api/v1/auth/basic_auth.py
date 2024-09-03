a90ed10cd96ddd70599023d59557148a65612633
bereket-kume <bereketkume@gmail.com>
2024-09-04 00:20:56 +0300

    basic user onject

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -57,17 +57,19 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str
     ) -> TypeVar("User"):
+        """
+        returns the user instance based on the email
+        and password
+        """
         if user_email is None or not isinstance(user_email, str) or\
                 user_pwd is None or not isinstance(user_pwd, str):
             return None
 
         try:
-            users = User.search({'email': user_email})
+            found_users = User.search({'email': user_email})
         except Exception:
             return None
-
-        if len(users) <= 0:
-            return None
-        if users[0].is_valid_password(user_pwd):
-            return users[0]
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
         return None
