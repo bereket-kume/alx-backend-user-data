bde3cd7c49dfee3ad5632c1406cff5e1597550db
bereket-kume <bereketkume@gmail.com>
2024-09-03 22:58:28 +0300

    Basic base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,12 @@ class BasicAuth(Auth):
     """
     Basic Auth Class
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+    ) -> str:
+        if authorization_header is None or\
+            not isinstance(authorization_header, str) or\
+                not authorization_header.startswith("Basic" + " "):
+            return None
+        else:
+            return authorization_header[6:]
