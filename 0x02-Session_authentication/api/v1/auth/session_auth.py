4f0cd4091804a5cb01daa83463aee45e000cddaf
bereket-kume <bereketkume@gmail.com>
2024-09-05 12:13:50 +0300

    create session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,23 @@
 Session Auth module
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Session Auth Class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create session as key of the dictionary
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
