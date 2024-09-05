852ea845744d43db23915910e2b7e7d8bcf7ea11
bereket-kume <bereketkume@gmail.com>
2024-09-05 13:09:00 +0300

    before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -44,7 +44,8 @@ def before_request():
         ]
 
     if auth.require_auth(request.path, ex_path):
-        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+        if auth.authorization_header(request) is None and\
+                auth.session_cookie(request) is None:
             abort(401)
         current_user = auth.current_user(request)
         if current_user is None:
