43612a84a20bfa7569c68ea5eccceb7041b28b7d
bereket-kume <bereketkume@gmail.com>
2024-09-07 23:59:52 +0300

    fix error

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
+""" UserSession module
 """
-user session model
-"""
-from .base import Base
+from models.base import Base
 
 
 class UserSession(Base):
     """
-    User session class
+    UserSession class
     """
+
     def __init__(self, *args: list, **kwargs: dict):
         """
-        init method for usersession
-        user_id = user_id
-        session_id = session_id
+        Initialize a UserSession instance
         """
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get("user_id")
-        self.session_id = kwargs.get("session_id")
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
